463ff86d0fe875954662db2a5f9a12e1e4ffa47d
Kumar Shubham <shubham2932@gmail.com>
2019-03-04 21:23:31 +0530

    add filter functionality

--- static/js/mscript.js
+++ static/js/mscript.js
@@ -1,17 +1,52 @@
 $(document).ready(function() {
 
-    build_html = function(response){
-        var ipr = 6;
-        mhtml = '<table class="table table-striped table-bordered table-hover">';
+    var questions_list = [];
+
+    matches = function(pattern, txt){
+        var len = pattern.length;
+        for(var i=0;i<len;i++){
+            if(pattern[i]!=txt[i]){
+                console.log(txt+" violated at "+i);
+                return 0;
+            }
+        }
+        console.log(txt+" MATCHES!");
+        return 1;
+    }
+
+    get_list = function(response){
         response = response.substring(1,response.length-1);
-        console.log(response);
+        //console.log(response);
         plist = response.split(',');
-        console.log(plist.length);
+        //console.log(plist.length);
+        qlist = []
+        for(var j=0;j<plist.length; j++){
+            var txt = plist[j];
+            txt = txt.substring(1+(j!=0),txt.length-1);    
+            qlist.push(txt);
+        }
+        return qlist;
+    }
+
+    filter_list = function(pattern){
+        console.log("filter list");
+        rlist = [];
+        for(var j=0;j<questions_list.length; j++){
+            var txt = questions_list[j];
+            if(matches(pattern.toUpperCase(),txt.toUpperCase()))
+                rlist.push(txt);
+        }
+        console.log("filter list returning "+rlist);
+        return rlist;
+    }
+
+    build_html = function(plist){
+        var ipr = 6;
+        mhtml= '<table class="table table-striped table-bordered table-hover">';
         for(var i=0;i<plist.length;i+=ipr){
             mhtml += '<tr>';
             for(var j=0;i+j<plist.length && j<ipr; j++){
                 var txt = plist[i+j];
-                txt = txt.substring(1+(i+j!=0),txt.length-1);    
                 mhtml+='<td><a href="https://www.spoj.com/problems/'+ txt +'" target="_blank">'+ txt +'</a></td>';
             }
             mhtml += '</tr>';
@@ -32,7 +67,8 @@ $(document).ready(function() {
                     $('#content').html("Bad id. Please check id.");
                 }
                 else{
-                    properhtml = build_html(response);
+                    questions_list = get_list(response);
+                    var properhtml = build_html(questions_list);
                     $('#content').html(properhtml);
                 }
             },
@@ -41,4 +77,16 @@ $(document).ready(function() {
             }
         });
     });
+
+    $("body").on("keyup","#pattern",function(event){
+        if ( event.which == 13 ) {
+            event.preventDefault();
+        }
+        var pattern = this.value;
+        console.log(pattern);
+        var filtered_list = filter_list(pattern);
+        var properhtml = build_html(filtered_list);
+        $('#content').html(properhtml);
+    });
+
 });
